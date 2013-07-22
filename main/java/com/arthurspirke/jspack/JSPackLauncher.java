ddc114a07fa21c79321895ebc82d035620c742c2
Arthur Spirke <arthur.spirke@gmail.com>
2013-07-22 19:43:29 +0300

    Simple refactor

--- main/java/com/arthurspirke/jspack/JSPackLauncher.java
+++ main/java/com/arthurspirke/jspack/JSPackLauncher.java
@@ -7,37 +7,34 @@ import java.util.ResourceBundle;
 
 public class JSPackLauncher {
 
-	
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-		
-		
-		ResourceBundle res = ResourceBundle.getBundle("com.arthurspirke.jspack.prop");
-	    String filesToPack = res.getString("filesToPack");
-	    String inputFolder = res.getString("inputFolder");
-	    String finalFile = res.getString("finalFile");
-	    String outputFolder = res.getString("outputFolder");
-	    
-	    File fileDirectory = new File(inputFolder);
-        File[] listOfAllFiles = fileDirectory.listFiles();
-        
+	public static void main(String[] args) throws FileNotFoundException,
+			IOException {
+
+		ResourceBundle res = ResourceBundle
+				.getBundle("com.arthurspirke.jspack.prop");
+		String filesToPack = res.getString("filesToPack");
+		String inputFolder = res.getString("inputFolder");
+		String finalFile = res.getString("finalFile");
+		String outputFolder = res.getString("outputFolder");
+
+		File[] listOfAllFiles = new File(inputFolder).listFiles();
         String[] listOfAllAvilableFiles = filesToPack.split(",");
         
-        PackWriter pw = new PackWriter(finalFile, outputFolder);
-        
-        
-        for(int i = 0; i < listOfAllAvilableFiles.length; i++){
-        	String file = listOfAllAvilableFiles[i];
-        	
-        	for(int j = 0; j < listOfAllFiles.length; j++){
-        		if(file.equals(listOfAllFiles[j].getName())){
-        			pw.writeFileInfo(listOfAllFiles[j]);
-        		}
-        	}
-        }
-        
-        pw.createNewFile();
-		
-		
+
+		PackWriter pw = new PackWriter(finalFile, outputFolder);
+
+		for (int i = 0; i < listOfAllAvilableFiles.length; i++) {
+			String file = listOfAllAvilableFiles[i];
+			System.out.println(file);
+			for (int j = 0; j < listOfAllFiles.length; j++) {
+				if (file.equals(listOfAllFiles[j].getName())) {
+					pw.writeFileInfo(listOfAllFiles[j]);
+				}
+			}
+		}
+
+		pw.createNewFile();
+
 	}
 
 }
